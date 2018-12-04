80f8e1c62f7eb036741d82153335d4ee2c2b92ed
Chiradeep <chiradeep.banerjee72>
2018-12-05 00:12:23 +0530

    Modified Algorithm of fizzBuzz

--- src/Array/FizzBuzzReverseBestPath.java
+++ src/Array/FizzBuzzReverseBestPath.java
@@ -14,145 +14,188 @@ import java.util.*;
  */
 public class FizzBuzzReverseBestPath {
     int fizzArr[], buzzArr[], fizzBuzzArr[];
-    HashMap<ArrayList<Integer>, Integer> myMap;
     int outputArr[];
     static String myFizzBuzz[];
+    boolean trueSet;
+    int howManyFizz = 0, howManyBuzz = 0, howManyFizzBuzz = 0;
 
 
     public static void main(String args[]) {
-        myFizzBuzz = new String[]{"fizz", "buzz", "fizzbuzz"};
+        myFizzBuzz = new String[]{"fizz", "buzz", "fizzbuzz", "fizz"};
         new FizzBuzzReverseBestPath().getReverseFizzBuzz(myFizzBuzz);
 
     }
 
-    private int[] getReverseFizzBuzz(String inputArr[]) {
+    int recursionVal = 0;
 
-        int fizzbuzzCount = 0;
-        myMap = new HashMap<>();
+    private int[] getReverseFizzBuzz(String inputArr[]) {
+        trueSet = true;
         ArrayList<Integer> myList = new ArrayList<>(inputArr.length);
 
-        //Find the number of "fizzbuzz" in the inputArr
-        int i = 0;
-        do {
-            if (inputArr[i] == "fizzbuzz") {
-                fizzbuzzCount++;
-            }
-            i++;
-        } while (i < inputArr.length);
-
-        System.out.println("FizzBuzz word count::" + fizzbuzzCount);
+        for (int i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] == "fizz")
+                howManyFizz++;
+            if (inputArr[i] == "buzz")
+                howManyBuzz++;
+            if (inputArr[i] == "fizzbuzz")
+                howManyFizzBuzz++;
+        }
 
         //Create Three diff arrays
         //15,30,45...fizzBuzz series.
-        int rangeLength = fizzbuzzCount * 15;
-
-        int fizzArrMaxLength = rangeLength / 3 - 1;
-        int buzzArrMaxLength = rangeLength / 5 - 1;
-        int fizzBuzzArrMaxLength = rangeLength / 15;
-
-        fizzArr = new int[fizzArrMaxLength];
-        buzzArr = new int[buzzArrMaxLength];
-        fizzBuzzArr = new int[fizzBuzzArrMaxLength];
-        int index = 1;
-        for (int j = 0; j < fizzArrMaxLength; j++) {
-
-            fizzArr[j] = 3 * index;
-            index++;
+        fizzArr = new int[100000];
+        buzzArr = new int[100000];
+        fizzBuzzArr = new int[100000];
+        int index = 0;
+        for (int j = 3; j < 100000; j++) {
+            int value = j;
+            if (value % 5 != 0 && value % 3 == 0 && value % 15 != 0) {
+                fizzArr[index] = value;
+                index++;
+            }
         }
-        index = 1;
-        for (int j = 0; j < buzzArrMaxLength; j++) {
-
-            buzzArr[j] = 5 * index;
-            index++;
+        index = 0;
+        for (int j = 5; j < 100000; j++) {
+            int value = j;
+            if (value % 5 == 0 && value % 3 != 0 && value % 15 != 0) {
+                buzzArr[index] = value;
+                index++;
+            }
         }
-        index = 1;
-        for (int j = 0; j < fizzBuzzArrMaxLength; j++) {
-
-            fizzBuzzArr[j] = 15 * index;
-            index++;
+        index = 0;
+        for (int j = 15; j < 100000; j++) {
+            int value = j;
+            if (value % 5 == 0 && value % 3 == 0 && value % 15 == 0) {
+                fizzBuzzArr[index] = value;
+                index++;
+            }
         }
 
-        callRecursively(inputArr, myList, fizzArrMaxLength, buzzArrMaxLength, fizzBuzzArrMaxLength, 1);
-
+        callRecursively(inputArr, myList,  0, 0, 0);
         return outputArr;
     }
 
-    private void callRecursively(String inputArr[], ArrayList<Integer> myList, int fizzArrMaxLength,
-                                 int buzzArrMaxLength, int fizzBuzzArrMaxLength, int globalIndex) {
-        int index = globalIndex;
-        myList = new ArrayList<>();
-        outputArr = decideShortestFizzBuzz(inputArr, inputArr.length);
-        for (int k = 0; k < outputArr.length; k++) {
-            myList.add(outputArr[k]);
-            System.out.println("elements Are::" + outputArr[k]);
-        }
-
-        myMap.put(myList, getMax(outputArr) - getMin(outputArr));
-
-
-        if (index == 1) {
-            index++;
-        }
-        //Here Recursive Call Happens
-
-        int localIndex = index;
-        fizzArr = new int[fizzArrMaxLength - 1];
-        for (int j = 0; j < fizzArrMaxLength - 1; j++) {
-
-            fizzArr[j] = 3 * localIndex;
-            localIndex++;
-        }
-
-        if (globalIndex > 0) {
-            localIndex = index;
-            buzzArr = new int[buzzArrMaxLength - 1];
-            for (int j = 0; j < buzzArrMaxLength - 1; j++) {
-
-                buzzArr[j] = 5 * localIndex;
-                localIndex++;
+    private void callRecursively(String inputArr[], ArrayList<Integer> myList, int fizzArrIndex,
+                                 int buzzArrIndex, int fizzbuzzArrIndex) {
+        try {
+            Set<Integer> myUniqueSet;
+            myList = new ArrayList<>();
+            for (int i = 0; i < inputArr.length; i++) {
+                if (inputArr[i] == "fizz") {
+                    myList.add(fizzArr[fizzArrIndex]);
+                    fizzArrIndex++;
+                } else if (inputArr[i] == "buzz") {
+                    myList.add(buzzArr[buzzArrIndex]);
+                    buzzArrIndex++;
+                } else if (inputArr[i] == "fizzbuzz") {
+                    myList.add(fizzBuzzArr[fizzbuzzArrIndex]);
+                    fizzbuzzArrIndex++;
+                }
             }
 
-            int fizzBuzzIndex = 1;
-            for (int j = 0; j < fizzBuzzArrMaxLength; j++) {
-
-                fizzBuzzArr[j] = 15 * fizzBuzzIndex;
-                fizzBuzzIndex++;
+            //Between list elements any other element is there ?
+            ArrayList<Integer> checkAnyOtherElements = new ArrayList<>();
+            for (int i = 0; i < myList.size(); i++) {
+                checkAnyOtherElements.add(myList.get(i));
             }
+            Collections.sort(checkAnyOtherElements);
+            int counter = 0;
+            if (inputArr.length == 3 ) {
+                for (int i = checkAnyOtherElements.get(0);
+                     i < checkAnyOtherElements.get(checkAnyOtherElements.size() - 1); i++) {
 
-            if (globalIndex <= buzzArrMaxLength) {
-                globalIndex++;
-                callRecursively(inputArr, myList, fizzArrMaxLength, buzzArrMaxLength, fizzBuzzArrMaxLength, globalIndex);
+                    //For fizz check
+                    if (i % 3 == 0 && i % 5 != 0 && i % 15 != 0) {
+                        counter++;
+                        trueSet = true;
+                    }
+
+                    //For buzz check
+                    if (i % 3 != 0 && i % 5 == 0 && i % 15 != 0) {
+                        counter++;
+                        trueSet = true;
+
+                    }
+                    //For fizzbuzz check
+                    if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                        counter++;
+                        trueSet = true;
+
+                    }
+                }
             } else {
+                for (int i = checkAnyOtherElements.get(0);
+                     i <= checkAnyOtherElements.get(checkAnyOtherElements.size() - 1); i++) {
+
+                    //For fizz check
+                    if (i % 3 == 0 && i % 5 != 0 && i % 15 != 0) {
+                        counter++;
+                        trueSet = true;
+                    }
+
+                    //For buzz check
+                    if (i % 3 != 0 && i % 5 == 0 && i % 15 != 0) {
+                        counter++;
+                        trueSet = true;
+
+                    }
+                    //For fizzbuzz check
+                    if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                        counter++;
+                        trueSet = true;
+
+                    }
+                }
+            }
+
+            //Check
+            if (counter > myList.size()) {
+                trueSet = false;
+            }
+
+            recursionVal++;
+            if (trueSet == false) {
+                fizzArrIndex = fizzArrIndex - howManyFizz;
+                buzzArrIndex = buzzArrIndex - howManyBuzz;
+                fizzbuzzArrIndex = fizzbuzzArrIndex - howManyFizzBuzz;
+                if (fizzArr[fizzArrIndex + 1] > buzzArr[buzzArrIndex]) {
+                    fizzArrIndex = fizzArrIndex + 1;
+                }
+                if (fizzArr[fizzArrIndex + 1] < buzzArr[buzzArrIndex]) {
+                    fizzArrIndex = fizzArrIndex + 1;
+                }
+
+                if (fizzArr[fizzArrIndex] > buzzArr[buzzArrIndex]) {
+                    buzzArrIndex = buzzArrIndex + 1;
+
+                }
+                if (fizzArr[fizzArrIndex] > fizzBuzzArr[fizzbuzzArrIndex]
+                        && buzzArr[buzzArrIndex] > fizzBuzzArr[fizzbuzzArrIndex]) {
+                    fizzbuzzArrIndex = fizzbuzzArrIndex + 1;
+                }
+                callRecursively(inputArr, myList, 100000, fizzArrIndex, buzzArrIndex, fizzbuzzArrIndex);
+            } else {
+                //Only for this particular iteration
+                myUniqueSet = new HashSet<>(myList);
+                Collections.sort(new ArrayList<Comparable>(myUniqueSet));
+                HashMap<ArrayList<Integer>, Integer> myMap = new HashMap<>();
+                myMap.put(new ArrayList<Integer>(myUniqueSet),
+                        Collections.max(myList) - Collections.min(myList));
+
                 Iterator it = myMap.entrySet().iterator();
                 while (it.hasNext()) {
                     Map.Entry pair = (Map.Entry) it.next();
-                    System.out.println("Shortest Path::" + pair.getKey() + " RouteValue: " + pair.getValue());
+                    System.out.println("Shortest Path::" + pair.getKey() + " Route Value: " + pair.getValue());
                 }
             }
+
+        } catch (StackOverflowError e) {
+            System.out.print("===Did not found===");
         }
 
 
     }
 
-    public static int getMax(int[] inputArray) {
-        int maxValue = inputArray[0];
-        for (int i = 1; i < inputArray.length; i++) {
-            if (inputArray[i] > maxValue) {
-                maxValue = inputArray[i];
-            }
-        }
-        return maxValue;
-    }
-
-    public static int getMin(int[] inputArray) {
-        int minValue = inputArray[0];
-        for (int i = 1; i < inputArray.length; i++) {
-            if (inputArray[i] < minValue) {
-                minValue = inputArray[i];
-            }
-        }
-        return minValue;
-    }
 
     private int[] decideShortestFizzBuzz(String inputArr[], int length) {
         int returnArr[] = new int[length];
